0c4fc83aa9b0dd50a618d655af9a5dfda3200bce
Shannon Poole <shannon.m.poole@gmail.com>
2022-01-22 21:05:36 -0500

    first commit

---
+++ DEVELOP.md
@@ -0,0 +1,11 @@
+# Clone repo and all submodules
+
+git clone --recurse-submodules git@github.com:revelryengine/platform.git
+
+# Requirements
+
+docker
+
+# To start dev server
+
+docker compose up -d
---
+++ dev.js
@@ -0,0 +1,45 @@
+import { Application, send } from 'https://deno.land/x/oak/mod.ts';
+
+const app = new Application();
+
+app.use(async (context) => {
+    console.log(context.request.url.pathname);
+
+    if(context.request.url.pathname.endsWith('/index.html')) {
+        return context.response.redirect(context.request.url.pathname.replace(/\/index.html$/, '/'));
+    }
+
+    const pattern = new URLPattern({ pathname: `/packages/*` });
+    if(pattern.test(context.request.url)) {
+        await send(context, context.request.url.pathname, {
+            root: `${Deno.cwd()}`
+        });
+    }
+
+    if(context.response.status === 404) {
+        await send(context, context.request.url.pathname, {
+            root: `${Deno.cwd()}/site`
+        });
+    }
+
+    if(context.response.status === 404) {
+        context.response.status = 200;
+        context.response.body = (await Deno.readTextFile('./site/index.html')).replace('<!-- {dev import map} -->', `
+        <!-- We can remove this shim once all major browsers support import maps -->
+        <script async src="https://ga.jspm.io/npm:es-module-shims@1.3.6/dist/es-module-shims.js"></script>
+        <script type="importmap">
+            {
+                "imports": {
+                    "https://cdn.jsdelivr.net/gh/revelryengine/": "/packages/"
+                }
+            }
+        </script>`);
+    }
+
+    context.response.headers.set('Access-Control-Allow-Origin', '*');
+});
+
+const port = Number(Deno.env.get('WEB_PORT')) || 8080;
+console.log(`listening on port ${port}`);
+
+await app.listen({ port });
