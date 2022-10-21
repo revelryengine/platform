88a7e7c0dd1730d5b2a453b24fb4e83d737f9d5d
Shannon Poole <shannon.m.poole@gmail.com>
2022-10-21 10:27:30 -0400

    Improved dev server and bumped sub modules

--- dev.js
+++ dev.js
@@ -1,36 +1,61 @@
 import { Application, send } from 'https://deno.land/x/oak/mod.ts';
 
-const app = new Application();
+const app = new Application({ logErrors: false });
 
-app.use(async (context) => {
-    console.log(context.request.url.pathname);
+app.use(async (context, next) => {
+    await next();
+    context.response.headers.set('Access-Control-Allow-Origin', '*');
+    console.log('%s - %s %s %s %s', 
+        new Date().toISOString(),
+        context.request.headers.get('X-Forwarded-For'),
+        context.response.status,
+        context.request.method,
+        context.request.url.pathname,
+    );
+});
 
+app.use(async (context, next) => {
     if(context.request.url.pathname.endsWith('/importmap.js')) {
         context.response.status = 200;
         context.response.headers.set('Content-Type', 'text/javascript');
         context.response.body   = (await Deno.readTextFile('./site/importmap.js')).replaceAll('https://cdn.jsdelivr.net/gh/revelryengine/', '/packages/');
-        return;
+    } else {
+        await next();
     }
+});
 
+app.use(async (context, next) => {
     if(context.request.url.pathname.endsWith('/index.html')) {
-        return context.response.redirect(context.request.url.pathname.replace(/\/index.html$/, '/'));
+        context.response.redirect(context.request.url.pathname.replace(/\/index.html$/, '/'));
+    } else {
+        await next();
     }
+});
 
+app.use(async (context, next) => {
     const pattern = new URLPattern({ pathname: `/packages/*` });
     if(pattern.test(context.request.url)) {
-        await send(context, context.request.url.pathname, {
-            root: `${Deno.cwd()}`
-        });
+        try {
+            await send(context, context.request.url.pathname, {
+                root: `${Deno.cwd()}`
+            });
+        } catch(e) {
+            await next();
+        }
+    } else {
+        await next();
     }
+});
 
-    if(context.response.status === 404) {
+app.use(async (context) => {
+    try {
         await send(context, context.request.url.pathname, {
             root: `${Deno.cwd()}/site`,
             index: 'index.html',
         });
+    } catch(e) {
+        
     }
-
-    context.response.headers.set('Access-Control-Allow-Origin', '*');
 });
 
 const port = Number(Deno.env.get('WEB_PORT')) || 8080;
