831f51a8877727095b209374169ef3d0c726e760
Shannon Poole <shannon.m.poole@gmail.com>
2023-12-27 08:58:27 -0500

    made sprite filter configurable

--- deps/renderer.js
+++ deps/renderer.js
@@ -1,1 +1,1 @@
-export * from 'https://cdn.jsdelivr.net/gh/revelryengine/renderer@v0.3.3-alpha/lib/renderer.js';
+export * from 'https://cdn.jsdelivr.net/gh/revelryengine/renderer@v0.3.4-alpha/lib/renderer.js';
