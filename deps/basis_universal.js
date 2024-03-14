91f904578a242b54c889917c7dc2a25e17c6fa82
Shannon Poole <shannon.m.poole@gmail.com>
2024-03-14 14:58:09 -0400

    improved worker handling

--- deps/basis_universal.js
+++ deps/basis_universal.js
@@ -1,4 +1,4 @@
-import BASIS from 'https://esm.sh/gh/BinomialLLC/basis_universal@1.16.4/webgl/transcoder/build/basis_transcoder.js?target=esnext';
+import BASIS from 'https://esm.sh/v135/gh/BinomialLLC/basis_universal@1.16.4/webgl/transcoder/build/basis_transcoder.js?target=esnext';
 
 export async function BasisUFactory() {
     const wasmBinary = await fetch(import.meta.resolve('https://cdn.jsdelivr.net/gh/BinomialLLC/basis_universal@1.16.4/webgl/transcoder/build/basis_transcoder.wasm')).then(res => res.arrayBuffer());
---
+++ deps/utils.js
@@ -0,0 +1,1 @@
+export * from 'https://cdn.jsdelivr.net/gh/revelryengine/utils@v0.3.2-alpha/lib/worker-helper.js';
